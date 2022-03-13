56cfae56d752ea9b1e8bd50614ec309695b2306c
Mohammed <mohdraza.syed@gmail.com>
2022-03-13 10:14:19 +0530

    website push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
               className="flex title-font font-medium items-center mb-4 md:mb-0"
               href="/"
             >
-              <span className="ml-3 text-xl">Mission Minded</span>
+              <span className="ml-3 text-xl"></span>
             </a>
             <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-xl justify-center">
               {/* <a className="ml-12 mr-12 hover:text-rose-400" href="/">
